391112665c0207488cebb5d545fc0819b17838cd
Pigor66 <higorribeirosantos@gmail.com>
2025-04-08 01:11:09 -0300

    Testing features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Avatar, Box, Button, Card, CardContent, Typography } from "@mui/material";
 import Link from "next/link";
 // import TranscriptionPage from "./pratice/page";
