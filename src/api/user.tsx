290ce5318058d6562379cfdcf38ba444a2a8bf94
Pigor66 <higorribeirosantos@gmail.com>
2025-05-12 00:56:45 -0300

    fix: ajust in req create user

--- src/api/user.tsx
+++ src/api/user.tsx
@@ -1,5 +1,5 @@
 import { User } from "@/types/user"
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 
 export const reqPost = async (body: User, query: string) => {
 
@@ -9,7 +9,14 @@ export const reqPost = async (body: User, query: string) => {
         password: body.password
     }
 
-    axios.post(`http://localhost:3000/${query}`, data)
-        .then(res => {return {data:res.data}})
-        .catch(err => {return {data:err}})
+    try {
+        const response = await axios.post(`http://localhost:3000/${query}`, data)
+        return {data: response.data, status: response.status}
+    } catch (error: unknown) {
+        const err = error as AxiosError;
+        return {data: null, error: err.response?.data, status: err.response?.status}
+    }
+
+
+
 }
--- src/app/(account)/register/page.tsx
+++ src/app/(account)/register/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import React, { ChangeEvent, FormEvent, useState, useEffect } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import {
-    Box,
-    Button,
-    TextField,
-    Typography,
-    Paper,
-    Stack,
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Paper,
+  Stack,
 } from '@mui/material';
 import BackgroundStars from '../../pratice/ParticleBackground';
 import { User } from '@/types/user';
@@ -17,119 +17,111 @@ import { reqPost } from '@/api/user';
 export default function RegisterForm() {
 
 
-    const [form, setForm] = useState<User>({
-        name: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
+  const [form, setForm] = useState<User>({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+
+
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
     });
+  };
 
-    const [passwordMatch, setPasswordMatch] = useState<boolean>(true);
-    const [prepareToSend, setPrepareToSend] = useState<boolean>(true);
+  const handleSubmit = async () => {
+    if (form.name && form.email && form.password && form.confirmPassword  && form.password === form.confirmPassword) {
+      try {
+        const response = await reqPost(form, "users");
+        console.log('Form Submitted:', response);
+      } catch (error) {
+        console.log('Error submitting form:', error);
+      }
+    } else {
+
+      console.log('Form not submitted', form);
+    }
 
 
-    useEffect(() => {
-        form.password === form.confirmPassword ? setPasswordMatch(true) : setPasswordMatch(false);
-        if (form.name && form.email && form.password && form.confirmPassword && passwordMatch) {
-            setPrepareToSend(true);
-        }else {
-            setPrepareToSend(false);
-        }
-        console.log(form);
-    }, [form]);
+  };
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setForm({
-            ...form,
-            [event.target.name]: event.target.value,
-        });
-    };
+  return (
 
-    const handleSubmit = async () => {
-        if (prepareToSend) {
-            
-            
-            const response = await reqPost(form, "users");
-            console.log('Form Submitted:', response);
-        } else {
-            console.log('Form not submitted', form);
-        }
-    };
+    <BackgroundStars>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
 
-    return (
+        <Paper elevation={3} sx={{ padding: 4, width: 400 }}>
+          <Typography variant="h5" gutterBottom textAlign={'center'}>
+            Login
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Stack spacing={2}>
+              <TextField
+                label="Nome"
+                name="name"
+                fullWidth
+                required
+                value={form.name}
+                onChange={handleChange}
+              />
+              <TextField
+                label="Email"
+                name="email"
+                type="email"
+                fullWidth
+                required
+                value={form.email}
+                onChange={handleChange}
+              />
+              <TextField
+                label="Senha"
+                name="password"
+                type="password"
+                fullWidth
+                required
+                value={form.password}
+                onChange={handleChange}
+              />
+              <TextField
+                label="Confirme sua senha"
+                name="confirmPassword"
+                type="password"
+                fullWidth
+                required
+                value={form.confirmPassword}
+                onChange={handleChange}
+              />
+              <Button onClick={handleSubmit}
+                variant="outlined"
+                size="large"
+                color="primary"
+                fullWidth>
+                Criar conta
+              </Button>
 
-        <BackgroundStars>
-            <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                minHeight="100vh"
-            >
-
-                <Paper elevation={3} sx={{ padding: 4, width: 400 }}>
-                    <Typography variant="h5" gutterBottom textAlign={'center'}>
-                        Login
-                    </Typography>
-                    <form onSubmit={handleSubmit}>
-                        <Stack spacing={2}>
-                            <TextField
-                                label="Nome"
-                                name="name"
-                                fullWidth
-                                required
-                                value={form.name}
-                                onChange={handleChange}
-                            />
-                            <TextField
-                                label="Email"
-                                name="email"
-                                type="email"
-                                fullWidth
-                                required
-                                value={form.email}
-                                onChange={handleChange}
-                            />
-                            <TextField
-                                label="Senha"
-                                name="password"
-                                type="password"
-                                fullWidth
-                                required
-                                error={!passwordMatch}
-                                value={form.password}
-                                onChange={handleChange}
-                            />
-                            <TextField
-                                label="Confirme sua senha"
-                                name="confirmPassword"
-                                type="password"
-                                fullWidth
-                                required
-                                error={!passwordMatch}
-                                value={form.confirmPassword}
-                                onChange={handleChange}
-                            />
-                            <Button onClick={handleSubmit}
-                                variant="outlined"
-                                size="large"
-                                color="primary"
-                                fullWidth>
-                                Criar conta
-                            </Button>
-
-                            <Button type="submit"
-                                variant="outlined"
-                                size="large"
-                                color="primary"
-                                href="/login"
-                                fullWidth>
-                                Voltar
-                            </Button>
-                        </Stack>
-                    </form>
-                </Paper>
-            </Box>
-        </BackgroundStars>
-    );
+              <Button type="submit"
+                variant="outlined"
+                size="large"
+                color="primary"
+                href="/login"
+                fullWidth>
+                Voltar
+              </Button>
+            </Stack>
+          </form>
+        </Paper>
+      </Box>
+    </BackgroundStars>
+  );
 }
 
